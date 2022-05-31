5d98829e90a408189320eb85440e9f3f63bf1cf3
holden1999 <Nightmare99>
2022-05-31 10:41:27 +0700

    alpha

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"os"
 )
 
 type Config struct {
@@ -13,11 +14,11 @@ type Config struct {
 
 func NewConfig() *Config {
 	config := new(Config)
-	dbHost := "127.0.0.1" //os.Getenv("DB_HOST")
-	dbPort := "3306"      //os.Getenv("DB_PORT")
-	dbUser := "root"      //os.Getenv("DB_USER")
-	dbPassword := "root"  //os.Getenv("DB_PASSWORD")
-	dbName := "pos"       //os.Getenv("DB_DBNAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_DBNAME")
 	config.RouterEngine = gin.Default()
 	config.ApiUrl = "localhost:3030"
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
--- server.go
+++ server.go
@@ -12,14 +12,14 @@ type Server interface {
 }
 
 type server struct {
-	routerEngine *gin.Engine
-	config       *config.Config
-	infra        manager.InfraManager
-	usecase      manager.UseCaseManager
+	routerEngine   *gin.Engine
+	config         *config.Config
+	infra          manager.InfraManager
+	useCaseManager manager.UseCaseManager
 }
 
 func (s *server) initHandlers() {
-
+	s.v1()
 }
 func (s *server) v1() {
 	//s.routerEngine.Use(middleware.AuthTokenMiddleware())
@@ -28,21 +28,21 @@ func (s *server) v1() {
 	productApiGroup := s.config.RouterEngine.Group("/products")
 	orderApiGroup := s.config.RouterEngine.Group("/orders")
 	reportApiGroup := s.config.RouterEngine.Group("/")
-	api.NewCategoryApi(categoryApiGroup, s.usecase.CategoryUseCase())
-	api.NewPaymentApi(paymentApiGroup, s.usecase.PaymentUseCase())
-	api.NewProductApi(productApiGroup, s.usecase.ProductUseCase())
-	api.NewOrderApi(orderApiGroup, s.usecase.OrderUseCase())
-	api.NewReportApi(reportApiGroup, s.usecase.ReportUseCase())
+	api.NewCategoryApi(categoryApiGroup, s.useCaseManager.CategoryUseCase())
+	api.NewPaymentApi(paymentApiGroup, s.useCaseManager.PaymentUseCase())
+	api.NewProductApi(productApiGroup, s.useCaseManager.ProductUseCase())
+	api.NewOrderApi(orderApiGroup, s.useCaseManager.OrderUseCase())
+	api.NewReportApi(reportApiGroup, s.useCaseManager.ReportUseCase())
 
 }
 
 func (s *server) v2() {
 	cashierApiGroup := s.config.RouterEngine.Group("/cashiers")
-	api.NewCashierApi(cashierApiGroup, s.usecase.CashierUseCase())
+	api.NewCashierApi(cashierApiGroup, s.useCaseManager.CashierUseCase())
 }
 
 func (s *server) Run() {
-
+	s.initHandlers()
 	err := s.config.RouterEngine.Run(s.config.ApiUrl)
 	if err != nil {
 		return
@@ -53,10 +53,10 @@ func NewApiServer() Server {
 	newConfig := config.NewConfig()
 	infra := manager.NewInfra(newConfig)
 	repo := manager.NewRepoManager(infra)
-	usecase := manager.NewUseCaseManager(repo)
+	UseCaseManager := manager.NewUseCaseManager(repo)
 	return &server{
-		config:  newConfig,
-		infra:   infra,
-		usecase: usecase,
+		config:         newConfig,
+		infra:          infra,
+		useCaseManager: UseCaseManager,
 	}
 }
