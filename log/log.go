01eddbdeb15f829d93554a8efa60cd34053bf88a
holden1999 <gunawanholden15@gmail.com>
2022-09-28 11:16:32 +0700

    get payment by id

--- log/log.go
+++ log/log.go
@@ -1,17 +1,1 @@
 package log
-
-import (
-	"github.com/rs/zerolog"
-	"os"
-)
-
-var Log *zerolog.Logger
-
-func New(loglevel string) {
-	zerolog.SetGlobalLevel(zerolog.ErrorLevel)
-	if loglevel == "debug" {
-		zerolog.SetGlobalLevel(zerolog.DebugLevel)
-	}
-	logger := zerolog.New(os.Stdout).With().Timestamp().Logger()
-	Log = &logger
-}
